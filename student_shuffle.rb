6c9e4b8e48a653135b64c4ca9ef2f66a52b531e4
Thomas Weld <thomas.weld@gmail.com>
2015-01-29 10:06:43 -0500

    new stuff

--- student_shuffle.rb
+++ student_shuffle.rb
@@ -6,5 +6,19 @@ students = ["Danielle", "Alesia", "Anita", "Ben", "Thomas", "Mathew", "Mark", "J
 puts students
 puts ""
 students.shuffle!
-puts students 
+
+
+
+
+count = 0
+num_students = students.length - 1
+
+until count == num_students
+
+	puts "#{students[count]} & #{students[count + 1]}"
+
+	count += 2
+
+end
+
 

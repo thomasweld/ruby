0242a0f460f864eaf01ee7c57cba3673d19fc8bc
Thomas Weld <thomas.weld@gmail.com>
2015-01-29 10:41:52 -0500

    get names from user

--- student_shuffle.rb
+++ student_shuffle.rb
@@ -1,24 +1,51 @@
 # student_shuffle.rb
 
+puts "Who's in class today?"
 
-students = ["Danielle", "Alesia", "Anita", "Ben", "Thomas", "Mathew", "Mark", "Jennifer", "Cameron"]
+students = Array.new
 
-puts students
+names = ""
+
+while names != "done"
+	names = gets.chomp
+	students.push(names)
+	
+end
+
+students.delete("done")
+
+
+# students = ["Danielle", "Alesia", "Anita", "Ben", "Thomas", "Mathew", "Mark", "Jennifer", "Cameron"]
+
+# puts students
 puts ""
 students.shuffle!
 
-
-
-
 count = 0
-num_students = students.length - 1
+num_students = students.length
 
 until count == num_students
 
-	puts "#{students[count]} & #{students[count + 1]}"
+#odd number of students ----
+	if num_students % 2 == 1
 
+		if count < num_students - 3
+
+			puts "#{students[count]} & #{students[count + 1]}"
+		
+		else
+
+			puts "#{students[count]} & #{students[count + 1]} & #{students[count + 2]}"
+			break
+		end
+
+#even number of students
+	else
+		puts "#{students[count]} & #{students[count + 1]}"		
+	end
+
+	
 	count += 2
-
 end
 
 
